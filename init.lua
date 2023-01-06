b1f9797c4af69696291f8431956ba4956718edba
marhone <no7nic3@gmail.com>
2023-01-06 11:02:56 +0800

    clean code

--- init.lua
+++ init.lua
@@ -1,11 +1,8 @@
 require("base.autoload_config")
 
--- require("app.windows")
 require("app.manipulation")
 require("app.battery")
 require("app.clipboard")
-
--- require("app.colorpicker")
 
 -- 测试脚本
 hs.hotkey.bind({"cmd", "alt"}, "t", function()
